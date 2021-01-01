8e357fc1d7d081e4ce1c77c3344c8efa91bc426b
wdvqmj7377 <hoangtri.45@gmail.com>
2021-01-01 15:18:14 +0900

    Update Design Main

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - UI components
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var lapsTableView: UITableView!
+    @IBOutlet weak var lapTimerLabel: UILabel!
+    @IBOutlet weak var lapRestButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
