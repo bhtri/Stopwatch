473af30eb9dedca06a55f5cb23859c8b21f705b0
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-01-09 17:50:47 +0900

    Update code

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    // MARK: - Variables
+    fileprivate let mainStopwatch: Stopwatch = Stopwatch()
+    fileprivate let lapStopwatch: Stopwatch = Stopwatch()
+    fileprivate var isPlay: Bool = false
+    fileprivate var laps: [String] = []
     
     // MARK: - UI components
     @IBOutlet weak var timerLabel: UILabel!
@@ -16,11 +21,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var lapRestButton: UIButton!
     @IBOutlet weak var playPauseButton: UIButton!
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - UI Settings
+    
+}
 
-
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return laps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier: String = "lapCell"
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        if let labelNum = cell.viewWithTag(11) as? UILabel {
+            labelNum.text = "Lap \(laps.count - (indexPath as NSIndexPath).row)"
+        }
+        
+        if let labelTimer = cell.viewWithTag(12) as? UILabel {
+            labelTimer.text = laps[laps.count - (indexPath as NSIndexPath).row - 1]
+        }
+        
+        return cell
+    }
+    
 }
 
